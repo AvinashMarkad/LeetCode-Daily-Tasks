67f241dd34845b40ee57cad5754bdfada6e3d5c6
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2024-08-15 21:22:03 +0530

    Lemonade Change

---
+++ lemonadeChange.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five > 0) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } else { // bill is 20
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}

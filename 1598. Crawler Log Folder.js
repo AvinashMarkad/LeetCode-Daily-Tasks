f98cdda68b03f55e695f36345af14c20eb3ebe69
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2024-08-22 20:52:42 +0530

    Update 1598. Crawler Log Folder.js

--- 1598. Crawler Log Folder.js
+++ 1598. Crawler Log Folder.js
@@ -1,17 +1,23 @@
 var minOperations = function(logs) {
     var depth = 0;
-    for (var i = 0; i < logs.length; i++) {
-        if (logs[i] == "../") {
-            if (depth > 0) {
-                depth -= 1;
-            } 
-        } else if (logs[i] == "./") {
-            depth += 0
-        } else {
-            depth += 1;
+    try {
+        for (var i = 0; i < logs.length; i++) {
+            if (logs[i] === "../") {
+                if (depth > 0) {
+                    depth -= 1;
+                } 
+            } else if (logs[i] === "./") {
+                // No change in depth
+            } else if (typeof logs[i] === "string") {
+                depth += 1;
+            } else {
+                throw new Error("Invalid log entry");
+            }
         }
-
+    } catch (error) {
+        console.error("An error occurred:", error.message);
+        return -1; // Return -1 or another value to indicate an error
     }
-    return depth < 0 ? 0 : depth;
 
+    return depth;
 };
